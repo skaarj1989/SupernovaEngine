a2d823439c000eb0e928d970f828044b0f1b4cdf
David <skaarj1989@gmail.com>
2023-10-12 18:47:08 +0200

    Add missing annotations

--- modules/Scripting/annotations/Scene.lua
+++ modules/Scripting/annotations/Scene.lua
@@ -8,3 +8,13 @@ function createEntity() end
 ---@return Entity
 ---@param id integer
 function getEntity(id) end
+
+---@return PhysicsWorld
+function getPhysicsWorld() end
+
+---@return AudioWorld
+function getAudioWorld() end
+
+---@param e integer # Entity ID
+---@overload fun(e: Entity)
+function setMainListener(e) end
