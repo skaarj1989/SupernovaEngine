23fa65b0a95450b146af56ba25a1e9a81380fbd1
David <skaarj1989@gmail.com>
2024-03-27 00:01:25 +0100

    [Scripting] Replace the hardcoded `LuaModules.hpp` with a dynamically generated header file

--- modules/Scripting/cmake/DefineScriptModule.cmake
+++ modules/Scripting/cmake/DefineScriptModule.cmake
@@ -14,3 +14,29 @@ function(DEFINE_SCRIPT_MODULE)
     set_target_properties(${TARGET_NAME} PROPERTIES FOLDER "Framework/ScriptingModules")
   endif()
 endfunction()
+
+function(ADD_BINDING HEADER_FILE REGISTER_FUNCTION)
+  set_property(GLOBAL APPEND PROPERTY LUA_MODULE_HEADER_FILES ${HEADER_FILE})
+  set_property(GLOBAL APPEND PROPERTY LUA_MODULE_REGISTRATION_FUNCTIONS ${REGISTER_FUNCTION})
+endfunction()
+
+function(CREATE_LUA_BINDINGS_HEADER_FILE)
+  get_property(HEADER_FILE_LIST GLOBAL PROPERTY LUA_MODULE_HEADER_FILES)
+  get_property(FUNCTION_LIST GLOBAL PROPERTY LUA_MODULE_REGISTRATION_FUNCTIONS)
+
+  set(SOURCE_CODE "#pragma once\n\n// Auto-generated file. Do not edit.\n\n")
+
+  foreach(HEADER_FILE IN LISTS HEADER_FILE_LIST)
+    string(APPEND SOURCE_CODE "#if __has_include(\"${HEADER_FILE}\")\n# include \"${HEADER_FILE}\"\n#endif\n")
+  endforeach()
+
+  string(APPEND SOURCE_CODE "\ninline void registerModules(sol::state &lua) {\n")
+
+  foreach(HEADER_FILE REGISTER_FUNCTION IN ZIP_LISTS HEADER_FILE_LIST FUNCTION_LIST)
+    string(APPEND SOURCE_CODE "#if __has_include(\"${HEADER_FILE}\")\n  ${REGISTER_FUNCTION}(lua)\;\n#endif\n")
+  endforeach()
+
+  string(APPEND SOURCE_CODE "}\n")
+
+  file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/../include/LuaModules.hpp ${SOURCE_CODE})
+endfunction()
